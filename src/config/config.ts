89f467a5c3898e734795e11d3054cb0a668c766c
Jhoiner243 <jhoinermartinez243@gmail.com>
2025-07-17 09:12:32 -0500

    chore: enhance configuration handling by introducing global window properties for Vite environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,18 @@
-export const { VITE_API_URL, VITE_VAPID_KEY, VITE_CLERK_PUBLISHABLE_KEY } =
-  import.meta.env;
+declare global {
+  interface Window {
+    __APP_CONFIG__?: {
+      VAPID_KEY?: string;
+      CLERK_PUBLISHABLE_KEY?: string;
+      API_URL?: string;
+    };
+  }
+}
+
+export const VITE_VAPID_KEY =
+  window.__APP_CONFIG__?.VAPID_KEY ||
+  "BEnPHW_WvNXu0i8dxYAIVjrINaCMafIwNXofeDPWXi2QUzRUSY63W6DOhqH84Im9mmM-BQHriKeFfpmfKcxwS5c";
+export const VITE_CLERK_PUBLISHABLE_KEY =
+  window.__APP_CONFIG__?.CLERK_PUBLISHABLE_KEY ||
+  "pk_test_cmFwaWQtbW9yYXktODcuY2xlcmsuYWNjb3VudHMuZGV2JA";
+export const VITE_API_URL =
+  window.__APP_CONFIG__?.API_URL || "http://localhost:8080";
