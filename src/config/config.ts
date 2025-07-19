385df6673b69d329df003604dbaf877d606b6f88
Jhoiner243 <jhoinermartinez243@gmail.com>
2025-07-19 15:18:27 -0500

    chore: update Clerk publishable key in configuration for production environment

--- src/config/config.ts
+++ src/config/config.ts
@@ -13,5 +13,5 @@ export const VITE_VAPID_KEY =
   "BEnPHW_WvNXu0i8dxYAIVjrINaCMafIwNXofeDPWXi2QUzRUSY63W6DOhqH84Im9mmM-BQHriKeFfpmfKcxwS5c";
 export const VITE_CLERK_PUBLISHABLE_KEY =
   window.__APP_CONFIG__?.CLERK_PUBLISHABLE_KEY ||
-  "pk_test_cmFwaWQtbW9yYXktODcuY2xlcmsuYWNjb3VudHMuZGV2JA";
+  "pk_live_Y2xlcmsuZmlsbHN0ZXAuc2l0ZSQ";
 export const VITE_API_URL = "https://server.fillstep.site";
